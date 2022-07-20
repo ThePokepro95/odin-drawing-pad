688611b4776d938c5e570eb1263f0e33c83cf32b
Lawrence Isla <pokepro95@gmail.com>
2022-07-19 19:31:26 -0700

    Add color and grid size selection

--- code.js
+++ code.js
@@ -1,4 +1,5 @@
 let gridSize = 16;
+let selectedColor = "black";
 padCreator(gridSize);
 
 
@@ -17,11 +18,36 @@ function padCreator(size){
             drawingPad.appendChild(square);
         }
     }
+    document.querySelectorAll('.square').forEach(function(item) {
+        item.addEventListener('mouseover', function(){
+            item.classList.add('activated');
+        });
+    });
 }
 
-document.querySelectorAll('.square').forEach(function(item) {
-    item.addEventListener('mouseover', function(){
-        item.classList.add('activated');
+function clearPad(){
+    document.querySelectorAll('.square').forEach(square => {
+        square.remove();
     });
+}
+
+document.getElementById('grid-generator').addEventListener('click', () => {
+    gridSize = document.getElementById('grid-input').value;
+    if (gridSize < 1 || gridSize > 100){
+        alert("Please enter a value between 1 and 100");
+        return;
+    }
+    clearPad();
+    padCreator(gridSize);
 });
 
+document.querySelectorAll('.color').forEach(function(item){
+    console.log(item.getAttributeNames());
+    item.addEventListener('click', function(){
+        let currentlySelected = document.querySelector('.selected-color');
+        currentlySelected.classList.remove('selected-color');
+        item.classList.add('selected-color');
+        const selectedColor = document.querySelector(':root');
+        selectedColor.style.setProperty('--current-color', item.getAttribute('id'));
+    });
+});
